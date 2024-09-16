a0414656f6043b272e0fe5a68b5ef187e0f5a143
conple21 <conple21@gmail.com>
2024-09-16 20:10:54 +0900

    (1) Update Player Controller (2) Add Highlight Interface

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,10 +3,53 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true; 
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime); 
+
+	CursorTrace(); 
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit; 
+	GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, CursorHit); 
+	if (!CursorHit.bBlockingHit) { return; }
+
+	LastActor = ThisActor; 
+	ThisActor = CursorHit.GetActor(); 
+
+	// Line Trace for Cursor. There are Several Scenarios
+	if (!LastActor)
+	{
+		if (ThisActor)
+		{
+			ThisActor->HighlightActor(); 
+		}
+	}
+	else
+	{
+		if (!ThisActor)
+		{
+			LastActor->UnHighlightActor(); 
+		}
+		else
+		{
+			if (LastActor != ThisActor)
+			{
+				LastActor->UnHighlightActor(); 
+				ThisActor->HighlightActor(); 
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -16,7 +59,7 @@ void AAuraPlayerController::BeginPlay()
 
 	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()); 
 	check(Subsystem); 
-	Subsystem->AddMappingContxet(AuraContext, 0); 
+	Subsystem->AddMappingContext(AuraContext, 0); 
 
 	bShowMouseCursor = true; 
 	DefaultMouseCursor = EMouseCursor::Default; 
@@ -27,3 +70,29 @@ void AAuraPlayerController::BeginPlay()
 	SetInputMode(InputModeData); 
 
 }
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent(); 
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent); 
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move); 
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>(); 
+	const FRotator Rotation = GetControlRotation(); 
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f); 
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X); 
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y); 
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y); 
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X); 
+	}
+}
+
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -7,6 +7,9 @@
 #include "AuraPlayerController.generated.h"
 
 class UInputMappingContext; 
+class UInputAction; 
+struct FInputActionValue; 
+class IEnemyInterface; 
 /**
  * 
  */
@@ -17,12 +20,22 @@ class AURA_API AAuraPlayerController : public APlayerController
 	
 public:
 	AAuraPlayerController(); 
+	virtual void PlayerTick(float DeltaTime) override; 
 
 protected:
 	virtual void BeginPlay() override; 
+	virtual void SetupInputComponent() override; 
 
 private:
 	UPROPERTY(EditAnywhere, Category = "Input")
 	TObjectPtr<UInputMappingContext> AuraContext; 
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction; 
+
+	void Move(const FInputActionValue& InputActionValue); 
 	
+	void CursorTrace(); 
+	TScriptInterface<IEnemyInterface> LastActor; 
+	TScriptInterface<IEnemyInterface> ThisActor;
 };
